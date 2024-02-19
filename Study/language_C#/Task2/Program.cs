364109b7eb991177d105270df514e3cda9de50b7
GinGL <Fayst@outlook.com>
2024-02-19 21:55:26 +0500

    add Study:Task2

--- Study/language_C#/Task2/Program.cs
+++ Study/language_C#/Task2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long I1 = 6;
+long sum2 = I1 + i1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = d1 + i1;
+
+int product1 = i1 * i2;
+long product2 = I1 * i1;
+double product3 = i1 * d1;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
+
+int quotien = i1 / i2;
+Console.WriteLine(quotien);
+
+d1 = 6;
+double quotien2 = i1 / d1;
+Console.WriteLine(quotien2);
