510cc962025fddaf0492ec97308518b0390d0722
GinGL <Fayst@outlook.com>
2024-02-19 22:36:27 +0500

    add Sudy:task4

---
+++ Study/language_C#/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 4;
+int b = 42;
+int c = 41;
+int d = 5;
+int e = 7;
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
