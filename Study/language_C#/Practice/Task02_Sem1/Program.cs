2f4fbc3a10fd5eb8567284202b4cf51662f5a136
GinGL <Fayst@outlook.com>
2024-02-20 18:09:01 +0500

    Решение 2 задачи

---
+++ Study/language_C#/Practice/Task02_Sem1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая на вход принимает
+//целое число N, а на выходе показывает все целые
+//числа в промежутке от -N до N.
+//Примеры
+//4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+//2 => -2, -1, 0, 1, 2
+
+Console.WriteLine("Ввудите целое число a");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num <= 0)
+{
+    Console.WriteLine("Некорректный ввод!");
+    return;
+}
+
+int count = -num;
+
+while (count <= num)
+{
+    Console.Write(count + " ");
+    count++; //count += 1
+}
