12f5da2d8e53ef4c065a817ebfb0bb782471bbe5
GinGL <Fayst@outlook.com>
2024-02-23 17:37:23 +0500

    Создали папк с домашним заданием

---
+++ Study/language_C#/Homework/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+//Внутри класса Answer напишите метод CompareNumbers, который на вход принимает два числа и выводит, какое число большее, а какое меньшее.
+public class Answer {
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        // Введите свое решение ниже
+    if(firstNumber == secondNumber){
+        Console.WriteLine("числа равны");
+    
+        
+    } else {
+        if (firstNumber > secondNumber){
+        Console.WriteLine("первое число больше");}
+        else{
+        Console.WriteLine("второе число больше");}}
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int firstNumber, secondNumber;
+
+        if (args.Length >= 2) {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            firstNumber = 11;
+            secondNumber = -10;
+        }
+
+        // Не удаляйте строки ниже
+        CompareNumbers(firstNumber, secondNumber);
+    }
+}
