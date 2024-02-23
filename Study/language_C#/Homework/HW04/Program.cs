41e5f5645d380c9fe1b50d2047f544bee1f5bff4
GinGL <Fayst@outlook.com>
2024-02-23 19:00:41 +0500

    Решили четвёртое задания

---
+++ Study/language_C#/Homework/HW04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Answer {
+    static void PrintEvenNumbers(int number)
+    {
+      // Введите свое решение ниже
+    int count = 1;
+    while (count <= number){
+        if (count % 2 == 0){
+            Console.Write(count + " ");
+            count++;
+        }
+        else count++;
+    }
+
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 6;
+        }
+
+        // Не удаляйте строки ниже
+        PrintEvenNumbers(number);
+    }
+}
