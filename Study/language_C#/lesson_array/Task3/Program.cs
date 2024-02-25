965ff6e3ad2e9fbb54a3c5135d0a62c4bee79d10
GinGL <Fayst@outlook.com>
2024-02-25 18:02:20 +0500

    Добавили Lesson_array:Task3

---
+++ Study/language_C#/lesson_array/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int i = 0;
+
+while(i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}
