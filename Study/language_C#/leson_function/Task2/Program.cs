07f87d94fb7a6ed9a782750513e09768ada2b41d
GinGL <Fayst@outlook.com>
2024-02-26 20:18:07 +0500

    add leson_function:Task2

---
+++ Study/language_C#/leson_function/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿void PrintSquares(int limit)//void == она не чего не возращает
+{
+    int i = 1;
+    while(i <= limit)
+    {
+        Console.Write($"{i*i} ");
+        i++;
+    }
+}
+
+PrintSquares(4);
+PrintSquares(5);
+PrintSquares(10);
