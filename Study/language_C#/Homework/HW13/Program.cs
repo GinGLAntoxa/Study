76d9de39d918084b19cadfad61e2aa9b8b4dd078
GinGL <Fayst@outlook.com>
2024-03-01 16:07:58 +0500

    add Homework:HW13

---
+++ Study/language_C#/Homework/HW13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//Задача 2: Подсчет количества четных 
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[10]; // Массив для хранения случайных чисел
+        Random random = new Random(); // Генератор случайных чисел
+        int evenCount = 0; // Счетчик четных чисел
+                           // Заполнение массива случайными трехзначными числами
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(100, 1000); // Случайное число от 100 до 999
+            Console.Write(numbers[i] + " "); // Вывод сгенерированного числа
+                                             // Проверка на четность и увеличение счетчика
+            if (numbers[i] % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+        // Вывод количества четных чисел в массиве
+        Console.WriteLine($"\n Количество четных чисел в массиве: {evenCount}");
+    }
+}
