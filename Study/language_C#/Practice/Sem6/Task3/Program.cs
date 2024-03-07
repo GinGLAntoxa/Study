da2cf5302dc7ca06603bc57a4f0c342ca45600f8
GinGL <Fayst@outlook.com>
2024-03-07 20:58:56 +0500

    add Sem6:Task3

---
+++ Study/language_C#/Practice/Sem6/Task3/Program.cs
@@ -0,0 +1,44 @@
+﻿//Считать строку с консоли, состоящую из латинских
+//букв в нижнем регистре. Выяснить, сколько среди
+//введённых букв гласных.
+//Пример
+//“hello” => 2
+//“world” => 1
+static char[] StringToCharArray(string str)
+{
+    char[] charsArray = new char[str.Length];
+    for (int i = 0; i < charsArray.Length; i++)
+    {
+        charsArray[i] = str[i];
+    }
+    return charsArray;
+}
+void PrintArray(char[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+static int GetCountValue(string str)
+{
+    int count = 0;
+    string vowels = "aoeyui";
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        char cyrrentchar = str[i];
+        for (int j = 0; j < vowels.Length; j++)
+        {
+            if (vowels[j] == cyrrentchar)
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+Console.WriteLine("Введите текст");
+string str = Console.ReadLine()!;
+
+Console.WriteLine(GetCountValue(str));
