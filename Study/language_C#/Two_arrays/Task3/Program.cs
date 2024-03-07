221c7e307de71099e32005e1d3fc54d8a5884cf3
GinGL <Fayst@outlook.com>
2024-03-07 19:59:48 +0500

    add Two_arrays:Task3

---
+++ Study/language_C#/Two_arrays/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿string GetLettersFromString(string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if(char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string resull = GetLettersFromString(str);
+Console.WriteLine(resull);
