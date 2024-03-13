ed8af9f044e58cd6cf1141a799d79cf974e9e0f4
GinGL <Fayst@outlook.com>
2024-03-13 19:37:34 +0500

    add Homework_assignments:task3

---
+++ Study/language_C#/Recursion/Homework_assignments/task3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+static void PrintArrayReverse(int[] array, int index)
+{
+    if (index < 0)
+        return;
+
+    Console.WriteLine(array[index]);
+    PrintArrayReverse(array, index - 1);
+}
+
+int[] array = { 1, 2, 3, 4, 5 };
+PrintArrayReverse(array, array.Length - 1);
